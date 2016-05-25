d37e7ebcfb568a0e79781fba708f728c2cbc9d5e
ZakRao <raouti.zakari@gmail.com>
2016-05-25 09:26:25 +0200

    Admin

--- src/AppUserBundle/Controller/BloquerController.php
+++ src/AppUserBundle/Controller/BloquerController.php
@@ -7,6 +7,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Security\Core\Authorization\AuthorizationChecker;
 use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorage;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
 
 
 class BloquerController extends Controller
@@ -22,6 +23,8 @@ class BloquerController extends Controller
       $user->setEnabled("0");
        $recipient = $user->getEmailCanonical();
 
+       $user->setRoles('ROLE_ADMIN');
+
       $messagemail = \Swift_Message::newInstance()
             ->setSubject('Vous avez demandé à bloquer votre compte ')
 
@@ -31,12 +34,13 @@ class BloquerController extends Controller
             ->setCharset('utf-8')
             ->setBody(
                 $this->container->get('templating')->render(
-                    'MessagerieBundle:Emails:notification_commentaire.html.twig'
+                    'MessagerieBundle:Emails:notification_bloquer.html.twig'
                 ),
                 'text/html'
             );
                             
             $this->container->get('mailer')->send($messagemail);
+      
 
       $em->persist($user);
       $em->flush();
@@ -48,7 +52,83 @@ class BloquerController extends Controller
   
   }
 
+  /**
+   * @Security("has_role('ROLE_ADMIN')")
+   */
+
+   public function bloquerbyadminAction (Request $request, $id)
+  {
+    $user = $this->getDoctrine()
+            ->getManager()->getRepository('AppUserBundle:User')->find($id);
+      $em = $this->getDoctrine()->getManager();
+
+      $user->setEnabled("0");
+       $recipient = $user->getEmailCanonical();
+
+      $messagemail = \Swift_Message::newInstance()
+            ->setSubject('Vous avez demandé à bloquer votre compte ')
+
+            //Mettre l'adresse mail des Heureuses naissances à la place de la mienne!!
+            ->setFrom($this->getParameter('mailer_user')) 
+            ->setTo($recipient)
+            ->setCharset('utf-8')
+            ->setBody(
+                $this->container->get('templating')->render(
+                    'MessagerieBundle:Emails:notification_bloquer.html.twig'
+                ),
+                'text/html'
+            );
+                            
+            $this->container->get('mailer')->send($messagemail);
+
+      $em->persist($user);
+      $em->flush();
+
+     
+
+      return $this->redirectToRoute('admin');
+
   
+  }
+
+  /**
+   * @Security("has_role('ROLE_ADMIN')")
+   */
+
+   public function debloquerbyadminAction (Request $request, $id)
+  {
+    $user = $this->getDoctrine()
+            ->getManager()->getRepository('AppUserBundle:User')->find($id);
+      $em = $this->getDoctrine()->getManager();
+
+      $user->setEnabled("1");
+       $recipient = $user->getEmailCanonical();
+
+      $messagemail = \Swift_Message::newInstance()
+            ->setSubject('Vous avez demandé à bloquer votre compte ')
+
+            //Mettre l'adresse mail des Heureuses naissances à la place de la mienne!!
+            ->setFrom($this->getParameter('mailer_user')) 
+            ->setTo($recipient)
+            ->setCharset('utf-8')
+            ->setBody(
+                $this->container->get('templating')->render(
+                    'MessagerieBundle:Emails:notification_bloquer.html.twig'
+                ),
+                'text/html'
+            );
+                            
+            $this->container->get('mailer')->send($messagemail);
+
+      $em->persist($user);
+      $em->flush();
+
+     
+
+      return $this->redirectToRoute('admin');
+
+  
+  }
 
 
 }
